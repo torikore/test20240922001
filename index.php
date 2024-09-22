9af33ab7f312c06aec4669010822a4197aa1c527
3aezu <57968946+torikore@users.noreply.github.com>
2024-09-22 17:51:23 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
 
+$servername = "localhost";
+$username = "root";
+$password = "sdf1sdf2";
+$dbname = "test20240922001";
 
-echo 'hello';
+// 接続を作成
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// 接続をチェック
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
+// SQLクエリを作成
+$sql = "select * from test001";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // データを出力
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " Created_at:" . $row["created_at"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
 ?>
 
